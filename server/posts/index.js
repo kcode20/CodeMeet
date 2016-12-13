890a6826642ed717938cd625d415ecfeb68e1247
eScribMac <evanscribner1@gmail.com>
2016-12-13 15:57:01 -0500

    search and delete

--- server/posts/index.js
+++ server/posts/index.js
@@ -1,7 +1,15 @@
 const posts = require('express').Router()
-
+const Posts = require('../../db/models/post');
 
 posts.use('/offers', require('./offers.route'));
 posts.use('/requests', require('./requests.route'));
+
+posts.get('/search', (req, res, next) => {
+
+    Posts.findByTag(req.query.search)
+        .then(pages => res.render('index', { pages }))
+        .catch(next);
+
+});
 
 module.exports = posts;
